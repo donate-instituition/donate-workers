aecf2999c6ec8d478ed4be4f32ba577769b392fe
Joao Lucas <joaolucas19982@gmail.com>
2026-08-10 18:01:26 -0300

    feat(workers): wire the new worker modules and Mongo/Redis into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import { RedisModule } from './cache';
+import { env } from './config/env';
 import { EmailModule } from './workers/email/email.module';
+import { NotificationPushModule } from './workers/notification-push/notification-push.module';
+import { ReceiptGenerateModule } from './workers/receipt-generate/receipt-generate.module';
+import { StripeWebhookModule } from './workers/stripe-webhook/stripe-webhook.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [
+    MongooseModule.forRoot(env.mongodbUri),
+    RedisModule,
+    EmailModule,
+    StripeWebhookModule,
+    ReceiptGenerateModule,
+    NotificationPushModule,
+  ],
 })
 export class AppModule {}
